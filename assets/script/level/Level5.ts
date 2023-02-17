726f6b934428e6008c47a28954c6496d9e1cdcf5
NguyenHuongGiang128 <studenthumg2000@gmail.com>
2023-02-17 15:27:30 +0700

    Update 17.02.2023.2
    done texture,pin,level3 remake

---
+++ assets/script/level/Level5.ts
@@ -0,0 +1,30 @@
+import { _decorator, Component, Node } from 'cc';
+import { LevelController } from '../controller/LevelController';
+import { Configs } from '../../utils/Configs';
+import { Pin } from '../P/Pin';
+const { ccclass, property } = _decorator;
+
+@ccclass('Level5')
+export class Level5 extends LevelController {
+    start() {
+        //set up parent raycast
+        this.setUpRaycastCallback((rayData)=>{
+            console.log(this.name,rayData);
+            for(let i = 0; i < rayData.length;i++){
+                console.log('ray',rayData[i].collider.node.name);
+                this.rayToNode(rayData[i].collider.node)
+            }
+        })
+        //
+    
+    }
+    //===============LEVEL LOGIC HERE!========================///
+    private rayToNode(whichNode:Node){
+        if(whichNode.name.includes(Configs.PIN_NAME)){
+            //pull the pin
+            whichNode.getComponent(Pin).onTouchMe();
+        }
+    }
+}
+
+
