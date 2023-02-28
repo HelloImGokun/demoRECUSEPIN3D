e31b0d56aba6eaf8cbf2fa24246187d3e42f27a6
NguyenHuongGiang128 <studenthumg2000@gmail.com>
2023-02-28 11:22:51 +0700

    fix lv1 ->9

--- assets/script/controller/GameController.ts
+++ assets/script/controller/GameController.ts
@@ -14,7 +14,7 @@ export class GameController extends Component {
     @property(Camera)
     private camera:Camera;
     //
-    private currentLevelNumber:number = 10;
+    private currentLevelNumber:number = 9;
     //
     @property(Node)
     private currentLevelNode:Node;
