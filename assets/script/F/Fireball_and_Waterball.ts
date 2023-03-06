5a588db72a34cf3971e4f99a7b3c5ab1939f26f9
NguyenHuongGiang128 <studenthumg2000@gmail.com>
2023-03-06 21:41:06 +0700

    update level 16

---
+++ assets/script/F/Fireball_and_Waterball.ts
@@ -0,0 +1,23 @@
+import { _decorator, Collider, Component, ITriggerEvent, Node } from 'cc';
+import { Configs } from '../../utils/Configs';
+const { ccclass, property } = _decorator;
+
+@ccclass('Fireball_and_Waterball')
+export class Fireball_and_Waterball extends Component {
+    start() {
+        let collider = this.getComponent(Collider);
+        collider.on('onTriggerStay', this.onTriggerStay, this);
+    }
+
+    private onTriggerStay(event: ITriggerEvent) {
+        let collisionNode: Node = event.otherCollider.node;
+        if(collisionNode.name.includes(Configs.PLAYER_NAME)||collisionNode.name.includes(Configs.KILL_ALL_OBJ)||collisionNode.name.includes(Configs.KILL_HUNTER)){
+            this.scheduleOnce(() => {
+                this.node.destroy();
+            }, 0.8);
+        }
+       
+    }
+}
+
+
