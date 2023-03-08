47c89b35f9aff34c6bc1c5b63d13f4f1d0373b31
hoantancong <hoantancong@gmail.com>
2023-03-08 15:21:20 +0700

    Merge branch 'van' of https://github.com/HelloImGiang/demoRECUSEPIN3D into van

--- assets/script/controller/GameController.ts
+++ assets/script/controller/GameController.ts
@@ -22,7 +22,7 @@ export class GameController extends Component {
     private currentLevelNode:Node;
     //
     onLoad(){
-        this.currentLevelNumber = 6;//Predata.instant.getSaveLevel();
+        this.currentLevelNumber = Predata.instant.getSaveLevel();
     }
     start() {   
 
