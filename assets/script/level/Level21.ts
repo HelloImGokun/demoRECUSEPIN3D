19fdd10a94ff22facbc683ad8879b8f584ffdc8c
NguyenHuongGiang128 <studenthumg2000@gmail.com>
2023-03-13 10:26:32 +0700

    level 21

---
+++ assets/script/level/Level21.ts
@@ -0,0 +1,24 @@
+import { _decorator, Component, Node } from 'cc';
+import { LevelController } from '../controller/LevelController';
+import { Pin } from '../P/Pin';
+import { Configs } from '../../utils/Configs';
+const { ccclass, property } = _decorator;
+
+@ccclass('Level21')
+export class Level21 extends LevelController {
+    start() {
+        //set up parent raycast callback
+        this.setUpRaycastCallback((rayData) => {
+          for (let i = 0; i < rayData.length; i++) {
+              console.log('ray', rayData[i].collider.node.name);
+              this.rayToNode(rayData[i].collider.node)
+          }
+      })          
+          }
+          private rayToNode(whichNode: Node) {
+              if (whichNode.name.includes(Configs.PIN_NAME)) {
+                  //pull the pin
+                  whichNode.getComponent(Pin).onTouchMe();
+              }
+          }
+      }
