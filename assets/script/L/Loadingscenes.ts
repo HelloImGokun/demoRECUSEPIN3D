40d9e7d37a4c071defdbda6f902924378db92a58
NguyenHuongGiang128 <studenthumg2000@gmail.com>
2023-03-14 17:08:38 +0700

    update preload , lv10

--- assets/script/L/Loadingscenes.ts
+++ assets/script/L/Loadingscenes.ts
@@ -1,18 +1,20 @@
 import { _decorator, Component, director, Node } from 'cc';
 import { Configs } from '../../utils/Configs';
+import { ResouceUtils } from '../../utils/ResouceUtils';
 const { ccclass, property } = _decorator;
 
 @ccclass('Loadingscenes')
 export class Loadingscenes extends Component {
-    start () {
+    start() {
         director.preloadScene(Configs.GAME_SCENES_PATH, () => {
-            
+            ResouceUtils.loadPrefab(Configs.LOAD_LEVEL_PATH + 1, (lvPrefab) => {
+            })
         },
-        () => {
-            director.loadScene(Configs.GAME_SCENES_PATH);
-        }
+            () => {
+                director.loadScene(Configs.GAME_SCENES_PATH);
+            }
         )
-        
+
     }
 }
 
--- assets/script/controller/GameController.ts
+++ assets/script/controller/GameController.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component,instantiate, Camera, input, Input, Vec2, geometry, PhysicsSystem, EventTouch, Node, tween, CCInteger, director, Game, sys } from 'cc';
+import { _decorator, Component, instantiate, Camera, input, Input, Vec2, geometry, PhysicsSystem, EventTouch, Node, tween, CCInteger, director, Game, sys } from 'cc';
 import { Configs } from '../../utils/Configs';
 import { ResouceUtils } from '../../utils/ResouceUtils';
 import { GameModel } from '../model/GameModel';
@@ -12,65 +12,69 @@ const { ccclass, property } = _decorator;
 @ccclass('GameController')
 export class GameController extends Component {
     @property(GameModel)
-    private gameModel:GameModel;
+    private gameModel: GameModel;
     @property(Camera)
-    private camera:Camera;
+    private camera: Camera;
     //
 
     //
 
-    private currentLevelNumber:number = 1;  
+    private currentLevelNumber: number = 1;
     //
     @property(Node)
-    private currentLevelNode:Node;
+    private currentLevelNode: Node;
     //
-    onLoad(){
-        this.currentLevelNumber =17;//Predata.instant.getSaveLevel();
+    onLoad() {
+        this.currentLevelNumber = Predata.instant.getSaveLevel();
     }
-    start() {   
+    start() {
 
         //create raycast
-        input.on(Input.EventType.TOUCH_START,this.onTouchScreen,this);
+        input.on(Input.EventType.TOUCH_START, this.onTouchScreen, this);
         //
-        input.on(Input.EventType.TOUCH_MOVE,this.onTouchMove,this);
-        input.on(Input.EventType.TOUCH_CANCEL,this.onTouchCancel,this);
+        input.on(Input.EventType.TOUCH_MOVE, this.onTouchMove, this);
+        input.on(Input.EventType.TOUCH_CANCEL, this.onTouchCancel, this);
         //set callback
         this.startLastLevel();
     }
 
-    private startLastLevel(){
-        this.gameModel.lbGameLevel.string = 'Level '+ this.currentLevelNumber;
+    private startLastLevel() {
+        this.gameModel.lbGameLevel.string = 'Level ' + this.currentLevelNumber;
         this.createNewLevel();
         //preload next level
-        let nextLevelPath = Configs.LOAD_LEVEL_PATH+(this.currentLevelNumber+1);
+        let nextLevelPath = Configs.LOAD_LEVEL_PATH + (this.currentLevelNumber + 1);
         ResouceUtils.preloadPrefab(nextLevelPath);
     }
-    public OnReload(){
+    public OnReload() {
         director.loadScene('game');
     }
-    public OnRestartalllevel(){
+    public OnRestartalllevel() {
         sys.localStorage.clear();
         Predata.instant.setSaveLevel(1);
         director.loadScene('game');
     }
-    private createNewLevel(){
-        ResouceUtils.loadPrefab(Configs.LOAD_LEVEL_PATH+this.currentLevelNumber,(lvPrefab)=>{
+    private createNewLevel() {
+        ResouceUtils.loadPrefab(Configs.LOAD_LEVEL_PATH + this.currentLevelNumber, (lvPrefab) => {
             //
             this.currentLevelNode = instantiate(lvPrefab);
-            this.currentLevelNode.getComponent(LevelController).setUp(()=>{
+            this.currentLevelNode.getComponent(LevelController).setUp(() => {
                 //win level event
                 this.winLevel();
-            },()=>{
+            }, () => {
                 //lose level
                 this.loseLevel();
             })
             this.gameModel.gamePlayNode.addChild(this.currentLevelNode);
+
+            ResouceUtils.loadPrefab(Configs.LOAD_LEVEL_PATH + (this.currentLevelNumber + 1), (lvPrefab) => {
+
+            })
         })
     }
-    private winLevel(){
+    private winLevel() {
         //    
         let winUI = instantiate(this.gameModel.winUIPrefab);
-        winUI.getComponent(WinUI).setUp(()=>{
+        winUI.getComponent(WinUI).setUp(() => {
             //onNext Level
             this.currentLevelNumber++;
             //save level
@@ -80,58 +84,58 @@ export class GameController extends Component {
             //
             this.nextLevel();
             //
-            
+
         })
 
         this.gameModel.canvasUI.addChild(winUI);
     }
     // loselevel
-    private loseLevel(){
+    private loseLevel() {
         let loseUI = instantiate(this.gameModel.loseUIPrefabs);
-        loseUI.getComponent(LoseUI).setUp(()=>{
+        loseUI.getComponent(LoseUI).setUp(() => {
             this.currentLevelNumber;
         })
         this.gameModel.canvasUI.addChild(loseUI);
     }
     //next level
-    private nextLevel(){
+    private nextLevel() {
         //
-        this.gameModel.lbGameLevel.string = 'Level '+ this.currentLevelNumber;
+        this.gameModel.lbGameLevel.string = 'Level ' + this.currentLevelNumber;
         this.createNewLevel();
         //
         this.preloadNextLevel();
     }
-    private preloadNextLevel(){
-            //preload to prepare next level to use later;
-            let nextLevelPath = Configs.LOAD_LEVEL_PATH+(this.currentLevelNumber+1);
-            ResouceUtils.preloadPrefab(nextLevelPath);
+    private preloadNextLevel() {
+        //preload to prepare next level to use later;
+        let nextLevelPath = Configs.LOAD_LEVEL_PATH + (this.currentLevelNumber + 1);
+        ResouceUtils.preloadPrefab(nextLevelPath);
     }
     //raycast handle
-    private onTouchScreen(touch: EventTouch){
-        let loc:Vec2 = touch.getLocation();
+    private onTouchScreen(touch: EventTouch) {
+        let loc: Vec2 = touch.getLocation();
         this.onRay(loc);
     }
-    private onTouchMove(touch: EventTouch){
-        let delta:Vec2 = touch.getDelta();
-        if(this.currentLevelNode!=null) {
+    private onTouchMove(touch: EventTouch) {
+        let delta: Vec2 = touch.getDelta();
+        if (this.currentLevelNode != null) {
             this.currentLevelNode.getComponent(LevelController).onMovePin(delta);
         }
     }
-    private onTouchCancel(touch: EventTouch){
-        if(this.currentLevelNode!=null) {
+    private onTouchCancel(touch: EventTouch) {
+        if (this.currentLevelNode != null) {
             this.currentLevelNode.getComponent(LevelController).onTouchCancel();
         }
     }
-    private onRay(position:Vec2){
+    private onRay(position: Vec2) {
         if (this.camera) {
             //create a ray through all collider;
             let _ray: geometry.Ray = new geometry.Ray();
             this.camera.screenPointToRay(position.x, position.y, _ray);
             const rayResult = PhysicsSystem.instance.raycastResults;
-            if (PhysicsSystem.instance.raycast(_ray)) {    
+            if (PhysicsSystem.instance.raycast(_ray)) {
                 //callback children 
                 //this.raycastCallback(rayResult);
-                if(this.currentLevelNode!=null) {
+                if (this.currentLevelNode != null) {
                     this.currentLevelNode.getComponent(LevelController).rayResult(rayResult);
                 }
             }
